5c69b9c07f011b28a6707b22ea169ed1f736699e
OnionSm <vuongha16104@gmail.com>
2024-04-30 17:38:45 +0700

    update skill4

--- Assets/Scripts/Skill/HailOfArrows.cs
+++ Assets/Scripts/Skill/HailOfArrows.cs
@@ -6,7 +6,11 @@ public class HailOfArrows : Skill
 {
     [SerializeField] public static HailOfArrows Instance;
     [SerializeField] private float arrow_speed;
-    private List<double> angle_shot;
+    private List<float> angle_shot;
+    private List<float> distances;
+    private List<float> time_arrow_fly;
+    private List<float> x_velo;
+    private List<float> y_velo;
 
     private void Awake()
     {
@@ -30,27 +34,41 @@ public class HailOfArrows : Skill
         PlayerAnimation.Instance.SetTriggerAngleShot();
         this.skill_time_count = this.skill_time;
         this.cool_down_time_count = this.cool_down;
-        foreach (float angle in angle_shot)
-        {
-            for (int i = 0; i <= 5; i++)
-            {
-                double random_angle = UnityEngine.Random.Range(angle - 3, angle + 3);
-                double y_velocity = Mathf.Tan(Mathf.Deg2Rad);
-                Transform arrow = BulletSpawner.Instance.Spawn(arrow_prefab_name, this.spawn_point.position, new Vector3(1, 1, 1));
-                arrow.gameObject.SetActive(true);
-                arrow.GetComponent<Rigidbody2D>().velocity = new Vector2(1, (float)y_velocity*5) * this.arrow_speed/3;
-            }
-        }
+        StartCoroutine(Skill());
     }
 
     protected override void LoadComponent()
     {
         this.arrow_prefab_name = "DropArrow";
-        this.angle_shot = new List<double>() { 45, 50, 60, 70, 80 };
+        this.angle_shot = new List<float>() {80, 75, 70, 65, 60};
         this.arrow_speed = 500f;
         this.cool_down = 1f;
         this.skill_time = 1.35f;
         this.skill_time_count = 0f;
         this.cool_down_time_count = 0f;
+        this.distances = new List<float>() {120, 210, 280, 330, 360};
+        this.time_arrow_fly = new List<float>() { 1.55f, 1.5f, 1.45f, 1.4f, 1.35f };
+        this.x_velo = new List<float>() { 80, 140, 187, 220, 240 };
+        this.y_velo = new List<float>() { 370, 350, 330, 310,290};
+    }
+    IEnumerator Skill()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+
+            for (int j = 0; j < 3; j++)
+            {
+                yield return new WaitForSeconds(0.005f);
+                float x_velocity = x_velo[i] + Random.Range(0,20);
+                float y_velocity = y_velo[i] + Random.Range(0, 20);
+                Transform arrow = BulletSpawner.Instance.Spawn(arrow_prefab_name, this.spawn_point.position, new Vector3(1, 1, 1));
+                arrow.gameObject.SetActive(true);
+                arrow.GetComponent<Rigidbody2D>().velocity = new Vector2(x_velocity, y_velocity);
+                arrow.GetComponent<ProjectileParticles>().Reload();
+                
+            }
+            yield return new WaitForSeconds(0.02f);
+        }
+        yield return null;
     }
 }
--- Assets/Scripts/Weapon/ProjectileParticles.cs
+++ Assets/Scripts/Weapon/ProjectileParticles.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class ProjectileParticles : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D particles;
+    private bool ground_trigger = false;
 
     public bool DestroyOnDisable;
-
     private Vector2 LocalPositions;
+
 
     private void Awake()
     {
+        this.particles = GetComponent<Rigidbody2D>();
+    }
+
+    protected void Update()
+    {
+        if(ground_trigger == false)
+            this.RotateArrow();
     }
 
     private void OnEnable()
@@ -20,5 +28,23 @@ public class ProjectileParticles : MonoBehaviour
 
     private void Disable()
     {
+    }
+    protected void RotateArrow()
+    {
+        float angle = Mathf.Atan2(particles.velocity.y, particles.velocity.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer != 3)
+            return;
+        particles.velocity = Vector2.zero;
+        ground_trigger = true;
+        particles.isKinematic = true;
+    }
+    public void Reload()
+    {
+        this.ground_trigger = false;
+        particles.isKinematic = false;
     }
 }
